c1bb0fc351407386ca8747aece4eb6e7b51c80c6
Muzir Khan <codingwithmk13@gmail.com>
2026-07-15 16:26:15 +0530

    Update App.jsx
    Added Analytics in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import axios from 'axios';
